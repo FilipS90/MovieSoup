3f8c73a2c016d82d9aa5ef7e8203677e2f9b098b
FilipS90 <filip.stojiljkovic90@yahoo.com>
2021-05-19 02:23:05 +0200

    Background and radio buttons

--- App/Gui.py
+++ App/Gui.py
@@ -1,13 +1,14 @@
 from tkinter import *
 from tkinter import ttk
 
-backgroundColor = 'orange'
-
 window = Tk()
 window.title('Претрага филмова')
 window.geometry('350x200')
 window.resizable(width=False, height=True)
-window.configure(background=backgroundColor)
+
+background=PhotoImage(file='ha.png')
+background_label = Label(window, image=background)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
 def runSearch():
     print('Blah')
@@ -16,7 +17,7 @@ def runSearch():
 entry = Entry(
     window,
     width=28)
-entry.grid(column=0, row=0, padx=5)
+entry.grid(column=0, row=0, padx=5, pady=(5,0))
 
 # Дугме за додавање кључних речи
 addMovie = Button(window,
@@ -26,7 +27,19 @@ addMovie = Button(window,
                 fg='white',
                 bg='blue',
                 command=runSearch)
-addMovie.grid(column=0, row=1, padx=5, pady=(3, 5))
+addMovie.grid(column=0, row=1, padx=5, pady=(3, 8))
+
+# Радио дугмићи
+var = IntVar()
+R1 = Radiobutton(window, text= 'По називу филма', variable=var, value=1,
+                command=runSearch)
+R1.configure(bg='blue', fg='white')
+R1.grid(column=0, row=2)
+
+R2 = Radiobutton(window, text= 'По жанру', variable=var, value=1,
+                command=runSearch)
+R2.configure(bg='blue', fg='white', width=14)
+R2.grid(column=0, row=3)
 
 # Дугме за претрагу
 searchButton = Button(window, text='Претражи', padx=20,
