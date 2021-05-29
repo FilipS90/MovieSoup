28b7d1b0c3562035fc62f202db528b405d5e6f4c
FilipS90 <filip.stojiljkovic90@yahoo.com>
2021-05-29 02:47:13 +0200

    IOUtils etc

---
+++ App/IOUtils.py
@@ -0,0 +1,66 @@
+import os.path
+import sys
+import importlib
+from pathlib import Path
+
+path = os.path.expanduser('~')
+os = sys.platform
+
+if os == 'win32':
+    path += '\\current_search.txt'
+else:
+    path += '/current_search.txt'
+
+def returnOrCreateFile():
+    filePath = Path(path)
+
+    if not filePath.is_file():
+        with open(path, 'w+') as nf:
+            pass
+
+    file = open(filePath, 'r')
+    fileAsString = file.read()
+    file.close()
+    return fileAsString
+
+def writeToFile(str):
+    with open(path, 'a') as f:
+        f.write(str)
+    # Gui = importlib.import_module('Gui')
+    # Gui.generateCurrentSearch()
+
+def addNewLine(line):
+    currentContent = returnOrCreateFile()
+
+    if currentContent:
+        writeToFile('\n'+line)
+    else:
+        writeToFile(line)
+
+def deleteLine(lineToDelete):
+    with open(path, 'r+') as f:
+        lines = f.readlines()
+        f.seek(0)
+        f.truncate()
+
+        for line in lines:
+            if line != lineToDelete:
+                f.write(line)
+    
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
